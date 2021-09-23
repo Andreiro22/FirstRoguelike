ed4b2ee06e2c70b643ad93dc2e20bdc1b1314117
Andreiro22 <91162059+Andreiro22@users.noreply.github.com>
2021-09-23 19:15:18 -0400

    Bound Movement Input

---
+++ FirstRoguelike/Source/FirstRoguelike/FirstRoguelike.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FirstRoguelike.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FirstRoguelike, "FirstRoguelike" );
---
+++ FirstRoguelike/Source/FirstRoguelike/FirstRoguelikeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FirstRoguelikeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTROGUELIKE_API AFirstRoguelikeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
